6b3e13e5c1b978017431e68de804b1cddb28a767
arttulankinen <arttu.lankinen@edu.savonia.fi>
2024-03-03 14:09:28 +0200

    pelivinkit to Arvostelut

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
-import {Historia} from './sivut/Historia';
-import {Pelivinkit} from './sivut/Pelivinkit';
-import {Tapahtumat} from './sivut/Tapahtumat';
-import {Pelitekniikka} from './sivut/Pelitekniikka';
+import { BrowserRouter as Router, Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Historia } from './sivut/Historia';
+import { Arvostelut } from './sivut/Arvostelut';
+import { Tapahtumat } from './sivut/Tapahtumat';
+import { Pelitekniikka } from './sivut/Pelitekniikka';
 import { Koti } from './sivut/Koti';
 
 
 function App() {
+
+  const handleSearch = (event) => {
+
+  }
+
+
   return (
     <Router>
-    <div>
-      <div id="header">
-        <h1>
-          <Link to="/"  id="otsikko">GamingGuides</Link>
+      <div>
+        <div id="header">
+          <h1>
+            <Link to="/" id="otsikko">GamingGuides</Link>
           </h1>
-        <div className="hakukentta">
-          <input type="text" placeholder="Hae..." />
-          <button type="hakunappi">Hae</button>
+          <div className="hakukentta">
+            <input type="text" placeholder="Hae..." />
+            <button type="hakunappi" onClick={handleSearch}>Hae</button>
+          </div>
         </div>
-      </div>
-      <div id="hakemisto" className="sticky">
-        <Link to="/Historia" className="button">Historia</Link>
-        <Link to="/Pelivinkit" className="button">Pelivinkit -ja arvostelut</Link>
-        <Link to="/Tapahtumat" className="button">Tapahtumat</Link>
-        <Link to="/Pelitekniikka" className="button">Pelitekniikka -ja arvostelut</Link>
-      </div>
+        <div id="hakemisto" className="sticky">
+          <Link to="/Historia" className="button">Historia</Link>
+          <Link to="/Arvostelut" className="button">Arvostelut</Link>
+          <Link to="/Tapahtumat" className="button">Tapahtumat</Link>
+          <Link to="/Pelitekniikka" className="button">Pelitekniikka</Link>
+        </div>
 
-      <div className='sisältö'>
-        <Routes>
-              <Route path="/" element={<Koti/>}/>
-              <Route path="/Historia" element={<Historia />} />
-              <Route path="/Pelivinkit" element={<Pelivinkit/>} />
-              <Route path="/Tapahtumat" element={<Tapahtumat/>} />
-              <Route path="/Pelitekniikka" element={<Pelitekniikka/>} />
-        </Routes>
-      </div>
+        <div className='sisältö'>
+          <Routes>
+            <Route path="/" element={<Koti />} />
+            <Route path="/Historia" element={<Historia />} />
+            <Route path="/Arvostelut" element={<Arvostelut />} />
+            <Route path="/Tapahtumat" element={<Tapahtumat />} />
+            <Route path="/Pelitekniikka" element={<Pelitekniikka />} />
+          </Routes>
+        </div>
 
-      <div id="footer">
-        <p>Arttu Lankinen © 2024</p>
-      </div>
+        <div id="footer">
+          <p>Arttu Lankinen © 2024</p>
+        </div>
 
-      <div id="background"></div>
-    </div>
-    </Router> 
+        <div id="background"></div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/sivut/Arvostelut.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Arvostelut() {
+  return (
+    <div className='container'>
+      <h2 id='otsikkoid'>Arvostelut</h2>
+
+    </div>
+  );
+}
+
+export { Arvostelut }
